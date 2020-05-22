814153c36fca1a0b5d8c65bc3cd191a26eab42cd
Mountech <myemail@email.com>
2020-05-22 10:25:49 +0300

    start of new programming course

--- app/index.php
+++ app/index.php
@@ -3,32 +3,12 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Полигон 45:00</title>
+    <title>Полигон</title>
     <link rel="stylesheet" href="css/style.min.css">
 </head>
 
 <body>
-    <form action="" class="fomrd">
-       имя
-        <input type="text" placeholder="Имя" name="name">
-        <span class="error_name"></span>
-        Email
-        <input type="text" placeholder="Email" name="email">
-        <span class="error_email"></span>
-        телефон
-        <input type="text" placeholder="Телефон" name="phone">
-        <span class="error_phone"></span>
-        <input type="button" value="отправить" class="send">
-    </form>
-    <br/>
-    <hr>
-    <div class="loader">
-        
-    </div>
-    <div class="result">
-        
-    </div>
-    <script src="js/libs.min.js"></script>
+   
     <script src="js/main.js"></script>
 </body>
 
--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,5 @@
-$(function(){
-    $('.send').on('click',function(){
-        let serverResponceSucess = 0;
-        let form = $('.fomrd');
-        let res = $('.result');
-        $('.loader').addClass('show');
-        $('.send').attr('disabled','disabled');
-        
-        $.ajax({
-            url:  "php/scripts.php",
-            method: "POST",
-            data: form.serialize(),
-            dataType: 'json',
-            success: function(data){
-                $('span[class*="error_"]').text('');
-                if(data.res){
-                    res.html("Заявка отправлена");
-                }
-                else{
-                    $('.send').removeAttr('disabled');
-                    console.log(data.errors);
-                    for (var key in data.errors) {
-                        $('span[class*="error_'+key+'"]').text(data.errors[key]);
-                    }
-                }
-            },
-            error: function(){
-                res.html("Сервер не отвечает");
-            },
-            compite: function(){
-                $('.loader').removeClass('show');
-            },
-        });
-        
-        
-        $.post('php/scripts.php',form.serialize(), function(data){
-            $('span[class*="error_"]').text('');
-            if(data.res){
-                res.html("Заявка отправлена");
-            }
-            else{
-                $('.send').removeAttr('disabled');
-                console.log(data.errors);
-                for (var key in data.errors) {
-                    $('span[class*="error_'+key+'"]').text(data.errors[key]);
-                }
-            }
-        },'json').always(function(){
-            $('.loader').removeClass('show');
-        }); 
-    });
-})
+window.onload = function(e){
+
+    
+
+}
